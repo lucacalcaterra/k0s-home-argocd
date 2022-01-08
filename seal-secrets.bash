74cab1bacc4e9adf7c5e6976fb3e0ba91add6111
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-01-08 12:33:00 +0000

    restore sealed secret override and update bash seal

--- seal-secrets.bash
+++ seal-secrets.bash
@@ -1,1 +1,1 @@
-kubeseal --format=yaml < ~/ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
