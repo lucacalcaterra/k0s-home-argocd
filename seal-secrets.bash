9d457e1e13edefebefe826a45812067fe36fb9bf
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-01-09 19:22:27 +0000

    traefik-forward-auth add test

--- seal-secrets.bash
+++ seal-secrets.bash
@@ -1,1 +1,2 @@
 kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/traefik-sso-secrets.yaml  > manifests/traefik-forward-auth/templates/traefik-sso-secrets-sealed.yaml
