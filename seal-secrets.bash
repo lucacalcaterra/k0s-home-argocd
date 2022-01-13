d24d1494a855e742197d08d316c193d78e8a6b8e
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-01-13 19:25:37 +0100

    cloudflare-ddns added

--- seal-secrets.bash
+++ seal-secrets.bash
@@ -1,2 +1,3 @@
 kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
 kubeseal --format=yaml < ../ArgoCD-Secrets/traefik-sso-secrets.yaml  > manifests/traefik-forward-auth/templates/traefik-sso-secrets-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-ddns-secrets.yaml  > manifests/cloudflare-ddns/cloudflare-ddns-secrets-sealed.yaml
