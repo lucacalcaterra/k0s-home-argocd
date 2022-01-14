f6d35b15d3a0fc91c9c4ecbd4586d2bb8e0d4641
Luca Calcaterra <calcaterra.luca@gmail.com>
2022-01-14 20:43:13 +0100

    test sealed secret for vpn config of qbittorrent

--- seal-secrets.bash
+++ seal-secrets.bash
@@ -1,3 +1,4 @@
 kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-api-credentials.yaml  > manifests/traefik-v2/templates/cloudflare-api-credentials-sealed.yaml
 kubeseal --format=yaml < ../ArgoCD-Secrets/traefik-sso-secrets.yaml  > manifests/traefik-forward-auth/templates/traefik-sso-secrets-sealed.yaml
 kubeseal --format=yaml < ../ArgoCD-Secrets/cloudflare-ddns-secrets.yaml  > manifests/cloudflare-ddns/cloudflare-ddns-secrets-sealed.yaml
+kubeseal --format=yaml < ../ArgoCD-Secrets/qbittorrent-vpn-config-secrets.yaml  > manifests/qbittorrent/templates/qbittorrent-vpn-config-secrets.yaml
